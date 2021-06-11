cc71624fe7d51848c34c9c0cb2ea95cac9a5e526
Dennis144 <63408052+Dennis144@users.noreply.github.com>
2021-06-11 17:25:31 +0200

    Software10

---
+++ DeepThought.java
@@ -0,0 +1,12 @@
+
+public class DeepThought {
+
+    public String answerToUltimateQuestionOfLifeTheUniverseAndEverything() {
+        return "41";
+    }
+
+    public static void main(String[] args) {
+        DeepThought dt = new DeepThought();
+        System.out.println(dt.answerToUltimateQuestionOfLifeTheUniverseAndEverything());
+    }
+}
